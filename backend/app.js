a37d2df90bd65b65a073781a5ce03f200825f812
Nikita <vershinin.nekita@yandex.ru>
2021-06-23 13:44:55 +0500

    Heroku fix

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,8 @@ app.use(cookieParser());
 
 let staticPath = path.join(__dirname, '../frontend/build');
 app.use(express.static(staticPath));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 const apiRouter = express.Router();
