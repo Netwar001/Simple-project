ade63d708b90e396f4edab36c6fcf8e72cc2a12c
Nikita <vershinin.nekita@yandex.ru>
2021-06-23 15:51:43 +0500

    fix priority

--- backend/app.js
+++ backend/app.js
@@ -15,15 +15,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-let staticPath = path.join(__dirname, '../frontend/build');
-app.use(express.static(staticPath));
-app.get('/ *', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
-
 const apiRouter = express.Router();
 app.use('/api', apiRouter);
 apiRouter.use('/sessions', indexRouter);
 apiRouter.use('/users', usersRouter);
+
+let staticPath = path.join(__dirname, '../frontend/build');
+app.use(express.static(staticPath));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 module.exports = app;
